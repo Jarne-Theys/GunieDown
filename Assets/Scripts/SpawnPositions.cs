4ca23018678b254be19cb60207e4a6cb067aa505
Jarne <jarne.theys@gmail.com>
2025-02-17 16:31:45 +0100

    Fix collider scale warnings

--- Assets/Scripts/SpawnPositions.cs
+++ Assets/Scripts/SpawnPositions.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SpawnPositions
 {
-    public static Vector3 humanPlayerSpawn = new Vector3(10, 1, 10);
-    public static Vector3 aiPlayerSpawn = new Vector3(10, 1, 20);
+    public static Vector3 humanPlayerSpawn = new Vector3(17, -9, 2);
+    public static Vector3 aiPlayerSpawn = new Vector3(25, -9, 27);
 
 }
