99de52a71f94e85f4eac0150ea8fdc1c32a60c4b
Jarne <jarne.theys@gmail.com>
2025-05-12 12:56:26 +0200

    add bullet collision file to git

---
+++ Assets/Scripts/BulletCollision.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour
+{
+    [HideInInspector]
+    public int bulletDamage;
+    public bool addRewardToAgentOnTargetHit = false;
+    public bool subtractRewardFromAgentOnAgentHit = false;
+    public bool subtractRewardFromAgentOnMiss = false;
+
+    public bool destroyOnPlayerContact;
+    public bool destroyOnTerrainContact;
+    
+    public AIPlayerAgent agent;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other == null)
+        {
+            Debug.LogError("other is null");
+            return;
+        }
+
+        if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Wall"))
+        {
+            if (destroyOnTerrainContact)
+            {
+                if (subtractRewardFromAgentOnMiss)
+                {
+                    agent.AddExternalReward(-0.01f, "Punished for hitting terrain");
+                }
+                Destroy(gameObject.transform.parent.gameObject);
+            }
+        }
+        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // TODO: make this method deal damage to the player hit
+            if (destroyOnPlayerContact)
+            {
+                if (addRewardToAgentOnTargetHit)
+                {
+                    agent.AddExternalReward(1f);
+                    //agent.EndEpisodeExternal("AI hit player!");
+                }
+
+                else
+                {
+                    PlayerStats playerStats = other.gameObject.GetComponent<PlayerStats>();
+                    playerStats.Damage(bulletDamage); 
+                }
+                
+                Destroy(gameObject.transform.parent.gameObject);
+            }
+        }
+        else if (other.gameObject.CompareTag("AIPlayer"))
+        {
+            if (destroyOnPlayerContact)
+            {
+                if (subtractRewardFromAgentOnAgentHit)
+                {
+                    agent.AddExternalReward(-0.5f, "Punished for getting hit");
+                }
+
+                else
+                {
+                    PlayerStats playerStats = other.gameObject.GetComponent<PlayerStats>();
+                    playerStats.Damage(bulletDamage);
+                }
+                
+                Destroy(gameObject.transform.parent.gameObject);
+            }
+        }
+
+        else
+        {
+            Destroy(gameObject.transform.parent.gameObject);
+        }
+    }
+}
